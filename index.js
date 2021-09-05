50437ebcb8586000ef8016c925bfaa37d0d2b767
DJEHA MOHAMED <38737729+dr4mohamed@users.noreply.github.com>
2021-09-05 20:10:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const redis = require("redis");
+const app = express();
+let redis_url = process.env.REDIS_HOST;
+let redis_port = process.env.REDIS_PORT;
+let client = require('redis').createClient("redis://"+redis_url+":"+redis_port);
+
+app.get("/", async (req, res) => {
+  client.get("title", (error, result)=> {                
+    if(error){                                                 
+      return res.status(500).json({error: error});                              
+    }else{
+      return res.status(200).json({result: result});                             
+    }    
+    
+  })  
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Node server started");
+});
+
